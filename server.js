2700bbe98a36ad21ce8ad82e1d6aba3faa5097e5
omar gamal <o.gamal.abdulzaher@gmail.com>
2022-03-19 14:37:46 +0200

    updating main code

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-import express from "express";
+import express from 'express';
+import productRouter from './routes/products.js'
+const app = express()
+
+app.use('/products', productRouter)
+app.listen(3000, console.log('listning on localhost 3000'))S
