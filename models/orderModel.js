09b5b806221658719c6624ed097a34fe8c88527e
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-04-23 22:31:51 +0200

    edited orderModel

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,8 +1,38 @@
+import { boolean } from "joi";
 import mongoose from "mongoose";
 
 
 const orderSchema = mongoose.Schema({
+    /* TODO:
+    //products of order
+    */
     user:{
         type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    totalPrice:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    paid:{
+        type:Boolean,
+        required:[true,"is paid must be specified"],
+        default:false
+    },
+    paymentMethod:{
+        type:String,
+        required:[true,"Payment method must be specified"],
+        enum:['card',"cash-on-delivery"]
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+
     }
-})
+
+});
+
+const Order = mongoose.Model('Order',orderSchema);
+
+export default Order;
