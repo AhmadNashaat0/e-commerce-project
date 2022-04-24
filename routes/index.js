7c3cc91ca97e737c24f0cc6c4559c9906f6de603
omar gamal <o.gamal.abdulzaher@gmail.com>
2022-04-24 08:39:58 +0200

    let app use cart routers

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import productsRoute from './products.js'
+import cartRouter from './cart.js'
 
 const routes =  (app) => {
     app.use('/api/products', productsRoute);
+    app.use('/api/cart', cartRouter);
 }
 
 export default routes
