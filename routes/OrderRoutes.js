4e949f7aa4ef27db240065ed611bd0308fd88458
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-04-24 17:20:33 +0200

    added order model routes

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -1,17 +1,22 @@
 import express from "express";
+import { createOrder, getAllOrders,getUserOrder,getOrderById,updateOrder, deleteOrder } from "../controllers/orderController.js";
 // import {} from ''
 
 const router = express.Router();
 
-
 router.
     route('/')
-    .get()
-    .post()
+    .get(getAllOrders)
+    .post(createOrder);
+
+router
+    .route('/myorders')
+    .get(getUserOrder)
+
 router.
     route('/:id')
-    .get()
-    .patch()
-    .delete()    
+    .get(getOrderById)
+    .patch(updateOrder)
+    .delete(deleteOrder);
 
 export default router;    
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import productsRoute from './products.js'
-
+import orderRoute from './OrderRoutes.js'
 const routes =  (app) => {
     app.use('/api/products', productsRoute);
+    app.use('/api/orders',orderRoute)
 }
 
 export default routes
