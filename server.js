8e71b02a36ca40088802f19538698583eb9a8b57
Ahmad Nashaat <ahmednashaat234@gmail.com>
2022-04-25 16:00:53 +0200

    add user schema and users routes

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ import express from "express";
 import { connectDB } from "./db/connect.js";
 import dotenv from 'dotenv'
 import routes from './routes/index.js';
+import cookies from 'cookie-parser';
+
 dotenv.config()
 
 const app = express();
 app.use(express.json());
+app.use(cookies());
 routes(app);
 
-connectDB(process.env.DATABASE_LOCAL)
+connectDB(process.env.DATABASE_CLOUD)
 app.listen(3000,()=>console.log('connected to the server on port 3000'))
