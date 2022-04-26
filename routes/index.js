8131d6e7e031e4669c7cd5d844f115be8fb320f6
Ahmad Nashaat <ahmednashaat234@gmail.com>
2022-04-27 00:14:00 +0200

    test heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,24 @@ import cartRouter from './cart.js';
 import usersRoute from './users.js';
 import orderRoute from './OrderRoutes.js';
 
+
 const routes = (app) => {
+    app.get('/', function(req, res){
+        res.render('../views/index.html');
+     });
+     app.get('/api', function(req, res){
+        res.render('../views/index.html');
+     });
     app.use('/api/products', productsRoute);
     app.use('/api/cart', cartRouter);
     app.use('/api/users', usersRoute);
     app.use('/api/orders',orderRoute);
+    app.get('*', function(req, res){
+        res.status(404).json({
+            status:'error',
+            message:"404 this Page Not Found"
+        });
+     });
 }
 
 export default routes
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cookies from 'cookie-parser';
 import dotenv from 'dotenv';
 import connectDB  from "./db/connect.js";
 import routes from './routes/index.js';
+import path from 'path';
+
 
 dotenv.config();
 
@@ -12,19 +14,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookies());
 app.use(express.static('views'));
-app.get('/', function(req, res){
-    res.render('views/index.html');
- });
-app.get('/api', function(req, res){
-    res.render('views/index.html');
- });
 routes(app);
-app.get('*', function(req, res){
-    res.status(404).json({
-        status:'error',
-        message:"404 this Page Not Found",
-    });
- });
+
+const mongo_url = process.env.MONGODB_URI || 'mongodb://database:27017' ;
 
 connectDB(process.env.DATABASE_CLOUD);
 app.listen(port, () => console.log(`Server works on port : ${port}`));
