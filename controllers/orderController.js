bd2fb5ec01a62be5d7a9b8d37434a050c4618143
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-04-26 16:37:12 +0200

    added error handlers for order models

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -17,66 +17,91 @@ const orders = await Order.find({})
 // @route   GET /api/orders/myorders
 // @access  Private
 export const getUserOrder = async(req,res,next) =>{
-    const user = req.user._id;
-    const orders = await Order.find({user})
-
-    res.status(200).json({
-        status:"Success",
-        count:orders.length,
-        data:orders
-    })
+    try {
+        const user = req.user._id;
+        const orders = await Order.find({user})
+    
+        res.status(200).json({
+            status:"Success",
+            count:orders.length,
+            data:orders
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            status:"fail",
+            data:error
+        })
+    }
 }
 
 // @desc    Get Order By id
 // @route   GET /api/orders/:id
 // @access  Private
 export const getOrderById = async(req,res,next)=>{
-    const orderId = req.params.id;
-    const order = await Order.findById(orderId);
 
-    if(!order){
-        // handle error
-    }
-
-    res.status(200).json({
-        status:"Success",
-        data:{
-            order
+    try {
+        const orderId = req.params.id;
+        const order = await Order.findById(orderId);
+    
+        if(!order){
+            throw new Error('No order found')
         }
-    })
+    
+        res.status(200).json({
+            status:"Success",
+            data:{
+                order
+            }
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            status:"Fail",
+            data:error
+        })
+    }
 }
 
 // @desc    Create Order
 // @route   POST /api/orders/
 // @access  Private
 export const createOrder = async (req,res,next) =>{
-    const user = req.user._id;
-    const {
-        cart,
-        totalPrice,
-        paid,
-        paymentMethod,
-        address
-    } = req.body;
-    if(!cart) // TODO: handle empty cart
-    {
-        // HANDLE ERROR
-    }
-
-    const newOrder = await Order.create({
-        user,
-        cart,
-        totalPrice,
-        paid,
-        paymentMethod,
-        address,
-    })
-    res.status(201).json({
-        status:"Success",
-        data:{
-            newOrder
+    try {
+        const user = req.user._id;
+        const {
+            cart,
+            totalPrice,
+            paid,
+            paymentMethod,
+            address
+        } = req.body;
+        if(!cart)
+        {
+            throw new Error("No cart found")
         }
-    })
+    
+        const newOrder = await Order.create({
+            user,
+            cart,
+            totalPrice,
+            paid,
+            paymentMethod,
+            address,
+        })
+        res.status(201).json({
+            status:"Success",
+            data:{
+                newOrder
+            }
+        });
+        
+    } catch (error) {
+        res.status(404).json({
+            status:"Fail",
+            data:error
+        })
+    }
 }
 
 // @desc    update an Order
@@ -84,6 +109,7 @@ export const createOrder = async (req,res,next) =>{
 // @access  Private
 export const updateOrder = async(req,res,next)=>
 {
+   try {
     const orderId = req.params.id;
     const toUpdate = req.body;
     const allowedUpdates = ["totalPrice","paid","paymentMethod","address"];
@@ -108,19 +134,26 @@ export const updateOrder = async(req,res,next)=>
             order
         }
     });
+   } catch (error) {
+    res.status(404).json({
+        status:"Fail",
+        data:error
+    })
+   }
 };
 
 // @desc    update an Order
 // @route   patch /api/orders/:id
 // @access  Private
 export const deleteOrder = async(req,res,next)=>{
+   try {
     const orderId = req.params.id;
     const order = await Order.findByIdAndDelete(orderId);
 
     if(!order)
     {
         //handle error
-        return next("");
+        throw new Error("can't find order with this ID")
     }
     res.status(200).json({
         status:"Success",
@@ -128,4 +161,10 @@ export const deleteOrder = async(req,res,next)=>{
             order
         }
     });
+   } catch (error) {
+    res.status(404).json({
+        status:"Fail",
+        data:error
+    });
+   }
 }
